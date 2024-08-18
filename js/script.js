fbe4c8f492e0ddb0cdad77f01d65d39ecdaaef7b
Valerianavo <lilaj2sy@gmail.com>
2024-08-18 16:06:51 -0500

    casi

--- js/script.js
+++ js/script.js
@@ -1,25 +1,60 @@
 const emojiCategories = {
-    animals: ['🐶', '🐱', '🦊', '🐻', '🐼', '🐨', '🐯', '🦁', '🐮', '🐷'],
-    food: ['🍎', '🍌', '🍇', '🍉', '🍒', '🍓', '🥑', '🍅', '🍔', '🍕'],
-    sports: ['⚽️', '🏀', '🏈', '⚾️', '🎾', '🏐', '🏉', '🎱', '🏓', '🏸'],
-    faces: ['😀', '😃', '😄', '😁', '😆', '😅', '😂', '🤣', '😇', '😉'],
+    'animals': ['🐶', '🐱', '🦊', '🐻', '🐼', '🐨', '🐯', '🦁', '🐮', '🐷', '🐵', '🐸', '🐔', '🐧', '🐦', '🐤', '🐢', '🐍', '🐳', '🐙', '🦑', '🐝', '🐞', '🦋', '🦓', '🐘', '🦒'],
+    'food': ['🍎', '🍌', '🍇', '🍉', '🍒', '🍓', '🥑', '🍅', '🍔', '🍕', '🌭', '🍟', '🍦', '🍰', '🍩', '🍪', '🍫', '🍭', '🍯', '☕️', '🍵', '🍻', '🍷', '🥂', '🥨', '🍣', '🥗'],
+    'sports': ['⚽️', '🏀', '🏈', '⚾️', '🎾', '🏐', '🏉', '🎱', '🏓', '🏸', '🏒', '🏑', '⛳️', '🥊', '🥋', '⛸', '🎿', '🏂', '🚴', '🏊', '🤽‍♂️', '🚣‍♀️', '🏌️‍♂️', '🎽', '🥇', '🏆', '🎯'],
+    'faces': ['😀', '😃', '😄', '😁', '😆', '😅', '😂', '🤣', '😇', '😉', '😊', '😋', '😍', '😘', '🥰', '😎', '🤩', '😏', '😒', '😔', '😕', '🙃', '🤑', '😲', '🤗', '🤓', '😛']
 };
 
+//function encryptMessage() {
+//    const inputText = document.getElementById('inputText').value;
+//    const category = document.getElementById('emojiCategory').value;
+//    const emojis = emojiCategories[category];
+//    const outputText = textToEmoji(inputText, emojis);
+//    document.getElementById('outputText').value = outputText;
+//}
+
+//function decryptMessage() {
+//    const inputText = document.getElementById('inputText').value;
+//    const category = document.getElementById('emojiCategory').value;
+//    const emojis = emojiCategories[category];
+//    const outputText = emojiToText(inputText, emojis);
+//    document.getElementById('outputText').value = outputText;
+//}
+
+//function textToEmoji(str, emojis) {
+//    return str.split('').map(char => {
+//        const index = char.charCodeAt() % emojis.length;
+//        return emojis[index];
+//    }).join('');
+//}
+
+//function emojiToText(str, emojis) {
+//    return str.split('').map(emoji => {
+//        const index = emojis.indexOf(emoji);
+//        return index !== -1 ? String.fromCharCode(index + 65) : emoji;
+//    }).join('');
+//}
+
+
 function encryptMessage() {
-    const inputText = document.getElementById('inputText').value;
+    const inputText = document.getElementById('inputText').value.trim().toLowerCase();
     const category = document.getElementById('emojiCategory').value;
     const emojis = emojiCategories[category];
     const outputText = textToEmoji(inputText, emojis);
-    document.getElementById('outputText').value = outputText;
+    document.getElementById('parrafo').innerText = outputText;
+    document.getElementById('titulo-mensaje').innerText = 'Mensaje Encriptado';
 }
 
+
 function decryptMessage() {
-    const inputText = document.getElementById('inputText').value;
+    const inputText = document.getElementById('inputText').value.trim();
     const category = document.getElementById('emojiCategory').value;
     const emojis = emojiCategories[category];
     const outputText = emojiToText(inputText, emojis);
-    document.getElementById('outputText').value = outputText;
+    document.getElementById('parrafo').innerText = outputText;
+    document.getElementById('titulo-mensaje').innerText = 'Mensaje Desencriptado';
 }
+
 
 function textToEmoji(str, emojis) {
     return str.split('').map(char => {
@@ -27,6 +62,7 @@ function textToEmoji(str, emojis) {
         return emojis[index];
     }).join('');
 }
+
 
 function emojiToText(str, emojis) {
     return str.split('').map(emoji => {
